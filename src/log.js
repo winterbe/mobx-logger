66e17522b899619a2716fa1fe04bebfceae5dd0f
Dmitriy Kolesnikov <xxsnakerxx@gmail.com>
2017-02-02 01:27:34 +0600

    Fixed action logging

--- src/log.js
+++ src/log.js
@@ -7,7 +7,7 @@ const style = (color, bold = true) => {
 const logAction = (ev) => {
     if (!ev.object) return;
 
-    console.groupCollapsed('%c%s  %s  %s.%s()', style('#000'), now(), padStart('ACTION', 8), ev.object, ev.name);
+    console.groupCollapsed('%c%s  %s  %s.%s()', style('#000'), now(), padStart('ACTION', 8), ev.object.name || ev.object, ev.name);
     console.log('%cFunction %o', style('#777'), ev.fn);
     console.log('%cArguments %o', style('#777'), ev.arguments);
     console.log('%cTarget %o', style('#777'), ev.object);
